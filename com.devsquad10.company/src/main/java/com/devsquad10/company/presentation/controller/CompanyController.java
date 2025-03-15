6dcb9f36b18b50ec8207f70158dac7039bf71b07
jjsh0208 <jjsh0208@gmail.com>
2025-03-15 19:06:16 +0900

    fix : #2 CompanyController LF 파일 변경

--- com.devsquad10.company/src/main/java/com/devsquad10/company/presentation/controller/CompanyController.java
+++ com.devsquad10.company/src/main/java/com/devsquad10/company/presentation/controller/CompanyController.java
@@ -1,1 +1,32 @@
-package com.devsquad10.company.presentation.controller;import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestBody;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;import com.devsquad10.company.application.dto.CompanyReqDto;import com.devsquad10.company.application.service.CompanyService;import lombok.RequiredArgsConstructor;@RestController@RequiredArgsConstructor@RequestMapping("/api/company")public class CompanyController {	private final CompanyService companyService;	@PostMapping	public ResponseEntity<?> createCompany(@RequestBody CompanyReqDto companyReqDto) {		companyService.createCompany(companyReqDto);		return ResponseEntity.status(HttpStatus.OK)			.body("Company successfully registered");	}}
+package com.devsquad10.company.presentation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsquad10.company.application.dto.CompanyReqDto;
+import com.devsquad10.company.application.service.CompanyService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/company")
+public class CompanyController {
+
+	private final CompanyService companyService;
+
+	@PostMapping
+	public ResponseEntity<?> createCompany(@RequestBody CompanyReqDto companyReqDto) {
+
+		companyService.createCompany(companyReqDto);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body("Company successfully registered");
+
+	}
+
+}
