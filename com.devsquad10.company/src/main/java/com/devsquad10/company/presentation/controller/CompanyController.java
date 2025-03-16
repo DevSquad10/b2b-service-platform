8533bdff00598434707d37895f095dbe13e8527b
jjsh0208 <jjsh0208@gmail.com>
2025-03-16 14:16:15 +0900

    refactor : #2 Company Response 제네릭 타입 수정

--- com.devsquad10.company/src/main/java/com/devsquad10/company/presentation/controller/CompanyController.java
+++ com.devsquad10.company/src/main/java/com/devsquad10/company/presentation/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.devsquad10.company.presentation.controller;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsquad10.company.application.dto.CompanyReqDto;
+import com.devsquad10.company.application.dto.CompanyResDto;
 import com.devsquad10.company.application.dto.CompanyResponse;
 import com.devsquad10.company.application.service.CompanyService;
 
@@ -28,7 +30,7 @@ public class CompanyController {
 	private final CompanyService companyService;
 
 	@PostMapping
-	public ResponseEntity<CompanyResponse> createCompany(@RequestBody CompanyReqDto companyReqDto) {
+	public ResponseEntity<CompanyResponse<CompanyResDto>> createCompany(@RequestBody CompanyReqDto companyReqDto) {
 
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(CompanyResponse.success(HttpStatus.OK.value(), companyService.createCompany(companyReqDto)));
@@ -36,13 +38,13 @@ public class CompanyController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<CompanyResponse> getCompanyById(@PathVariable("id") UUID id) {
+	public ResponseEntity<CompanyResponse<CompanyResDto>> getCompanyById(@PathVariable("id") UUID id) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(CompanyResponse.success(HttpStatus.OK.value(), companyService.getCompanyById(id)));
 	}
 
 	@GetMapping("/search")
-	public ResponseEntity<CompanyResponse> searchCompanies(
+	public ResponseEntity<CompanyResponse<Page<CompanyResDto>>> searchCompanies(
 		@RequestParam(required = false) String q,
 		@RequestParam(required = false) String category,
 		@RequestParam(defaultValue = "0") int page,
@@ -57,14 +59,14 @@ public class CompanyController {
 	}
 
 	@PatchMapping("/{id}")
-	public ResponseEntity<CompanyResponse> updateCompany(@PathVariable("id") UUID id,
+	public ResponseEntity<CompanyResponse<CompanyResDto>> updateCompany(@PathVariable("id") UUID id,
 		@RequestBody CompanyReqDto companyReqDto) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(CompanyResponse.success(HttpStatus.OK.value(), companyService.updateCompany(id, companyReqDto)));
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<CompanyResponse> deleteCompany(@PathVariable("id") UUID id) {
+	public ResponseEntity<CompanyResponse<String>> deleteCompany(@PathVariable("id") UUID id) {
 		companyService.deleteCompany(id);
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(CompanyResponse.success(HttpStatus.OK.value(), "Company Deleted successfully"));
