3fc486671a05771e05a04308e34984e7e7b3c3b7
jjsh0208 <jjsh0208@gmail.com>
2025-03-16 17:51:11 +0900

    refactor : #10 Product exception 디렉터리 오타 수정

--- com.devsquad10.product/src/main/java/com/devsquad10/product/application/exception/ProductNotFoundException.java
+++ com.devsquad10.product/src/main/java/com/devsquad10/product/application/exception/ProductNotFoundException.java
@@ -1,4 +1,4 @@
-package com.devsquad10.product.application.excption;
+package com.devsquad10.product.application.exception;
 
 public class ProductNotFoundException extends RuntimeException {
 	public ProductNotFoundException(String message) {
