a98dba84f266c595b59ceae82c92913194e1e1d3
aerhergag00 <jwlee101500@gmail.com>
2025-03-17 02:20:26 +0900

    feat: #19 Hub 단일 조회 구현

--- com.devsquad10.hub/src/main/java/com/devsquad10/hub/domain/repository/HubRepository.java
+++ com.devsquad10.hub/src/main/java/com/devsquad10/hub/domain/repository/HubRepository.java
@@ -1,7 +1,12 @@
 package com.devsquad10.hub.domain.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import com.devsquad10.hub.domain.model.Hub;
 
 public interface HubRepository {
 	Hub save(Hub hub);
+
+	Optional<Hub> findById(UUID id);
 }
