52d40f6d412028430e162dc17711d16ed9f44cdf
aerhergag00 <jwlee101500@gmail.com>
2025-03-17 01:26:28 +0900

    feat: #18 Hub 등록 구현

--- com.devsquad10.hub/src/main/java/com/devsquad10/hub/application/service/HubService.java
+++ com.devsquad10.hub/src/main/java/com/devsquad10/hub/application/service/HubService.java
@@ -1,4 +1,32 @@
 package com.devsquad10.hub.application.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsquad10.hub.domain.model.Hub;
+import com.devsquad10.hub.domain.repository.HubRepository;
+import com.devsquad10.hub.presentation.req.HubCreateRequestDto;
+import com.devsquad10.hub.presentation.res.HubCreateResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class HubService {
+
+	private final HubRepository hubRepository;
+
+	public HubCreateResponseDto createHub(HubCreateRequestDto request) {
+		Hub hub = Hub.builder()
+			.name(request.getName())
+			.address(request.getAddress())
+			.latitude(request.getLatitude())
+			.longitude(request.getLongitude())
+			.build();
+
+		Hub savedHub = hubRepository.save(hub);
+
+		return HubCreateResponseDto.toResponseDto(savedHub);
+	}
 }
