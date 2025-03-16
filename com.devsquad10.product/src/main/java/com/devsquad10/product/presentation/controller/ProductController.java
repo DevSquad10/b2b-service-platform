e8ae5af2bceb7310cf9e3388bdea8f8dc6dddb41
jjsh0208 <jjsh0208@gmail.com>
2025-03-16 16:23:20 +0900

    feat : #10 Product searchProducts 구현
    - '/api/product/search' 경로의 GET 요청을 처리
    - 검색 기능 추가: 쿼리 파라미터로 `q`, `category`, `page`, `size`, `sort`, `order`를 받아 상품 정보 검색
    - 허브 기준으로 검색기능 추가
    - 업체 기준으로 검색기능 추가
    - 페이징 처리 및 정렬 기능 구현

--- com.devsquad10.product/src/main/java/com/devsquad10/product/presentation/controller/ProductController.java
+++ com.devsquad10.product/src/main/java/com/devsquad10/product/presentation/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.devsquad10.product.presentation.controller;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsquad10.product.application.dto.ProductReqDto;
@@ -39,4 +41,19 @@ public class ProductController {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(ProductResponse.success(HttpStatus.OK.value(), productService.getProductById(id)));
 	}
+
+	@GetMapping("/search")
+	public ResponseEntity<ProductResponse<Page<ProductResDto>>> searchProducts(
+		@RequestParam(required = false) String q,
+		@RequestParam(required = false) String category,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(defaultValue = "createdAt") String sort,
+		@RequestParam(defaultValue = "desc") String order) {
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(ProductResponse.success(HttpStatus.OK.value(),
+				productService.searchProducts(q, category, page, size, sort, order)));
+	}
+
 }
