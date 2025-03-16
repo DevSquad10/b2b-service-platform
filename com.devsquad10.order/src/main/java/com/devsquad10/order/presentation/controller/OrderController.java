3401814c4f015244fd6cb881a775a6705765e627
jjsh0208 <jjsh0208@gmail.com>
2025-03-16 23:05:00 +0900

    feat : #25 Order create 기능 구현
    - '/api/order' 경로의 POST 요청을 처리
    - 주문 최초 생성 시 Redis에 저장
    - 주문 처리 완료 후 DB에 저장

---
+++ com.devsquad10.order/src/main/java/com/devsquad10/order/presentation/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.devsquad10.order.presentation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsquad10.order.application.dto.OrderReqDto;
+import com.devsquad10.order.application.dto.OrderResponse;
+import com.devsquad10.order.application.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+
+	private final OrderService orderService;
+
+	@PostMapping
+	public ResponseEntity<OrderResponse<String>> createOrder(@RequestBody OrderReqDto orderReqDto) {
+
+		orderService.createOrder(orderReqDto);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(OrderResponse.success(HttpStatus.OK.value(), "test"));
+	}
+}
