9c97674a7b2486374b33c48879546db7f2320f62
jjsh0208 <jjsh0208@gmail.com>
2025-03-18 00:49:52 +0900

    fix: #25 Order OrderExceptionHandler 핸들러 수정
    - OrderNotFoundException 수정

--- com.devsquad10.order/src/main/java/com/devsquad10/order/presentation/handler/OrderExceptionHandler.java
+++ com.devsquad10.order/src/main/java/com/devsquad10/order/presentation/handler/OrderExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.devsquad10.order.application.dto.response.OrderResponse;
+import com.devsquad10.order.application.exception.OrderNotFoundException;
 
 @RestControllerAdvice
 public class OrderExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(OrderNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ResponseEntity<OrderResponse<String>> OrderNotFoundException(Exception ex) {
+	public ResponseEntity<OrderResponse<String>> orderNotFoundException(OrderNotFoundException ex) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 			.body(OrderResponse.failure(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
 	}
