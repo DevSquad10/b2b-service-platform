9b9e0b6c2c1e5998accf1d89d90e406f6cf471b6
jjsh0208 <jjsh0208@gmail.com>
2025-03-18 00:06:11 +0900

    feat: #2 Company RedisConfig 변경
    - 각 캐시 키에 대해 TTL(Time To Live)을 다르게 설정
      - 단일 조회: 10분
      - 전체 리스트 검색: 1시간
    - 직렬화 방식을 JSON에서 이진 직렬화로 변경하여 캐싱 성능 개선
      - 이진 직렬화는 사람이 읽을 수 없다는 단점이 있음

--- com.devsquad10.company/src/main/java/com/devsquad10/company/infrastructure/config/redis/RedisConfig.java
+++ com.devsquad10.company/src/main/java/com/devsquad10/company/infrastructure/config/redis/RedisConfig.java
@@ -9,26 +9,58 @@ import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
+	public static final String COMPANY_CACHE = "companyCache";
+	public static final String COMPANY_SEARCH_CACHE = "companySearchCache";
+
+	private static final Duration DEFAULT_TTL = Duration.ofSeconds(120);
+	private static final Duration COMPANY_TTL = Duration.ofMinutes(10);
+	private static final Duration COMPANY_SEARCH_TTL = Duration.ofHours(1);
+
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		// 이진 직렬화 방식 적용
+		JdkSerializationRedisSerializer redisSerializer = new JdkSerializationRedisSerializer();
 
-		RedisCacheConfiguration configuration = RedisCacheConfiguration
+		// 기본값 캐시 설정 (2분 TTL, JSON 직렬화 방식)
+		RedisCacheConfiguration defaultConfiguration = RedisCacheConfiguration
 			.defaultCacheConfig()
 			.disableCachingNullValues()
-			.entryTtl(Duration.ofSeconds(120)) // 기본 유지 시간 120초
+			.entryTtl(DEFAULT_TTL) // 기본 유지 시간 120초
 			.computePrefixWith(CacheKeyPrefix.simple())
-			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.java()));
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+
+		// companyCache 에 대한 캐시 설정 (10분 TTL, JSON 직렬화 방식)
+		RedisCacheConfiguration companyConfiguration = RedisCacheConfiguration
+			.defaultCacheConfig()
+			.disableCachingNullValues()
+			.entryTtl(COMPANY_TTL)
+			.computePrefixWith(CacheKeyPrefix.simple())
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+
+		// companySearchCache 에 대한 캐시 설정 (1시간 TTL, JSON 직렬화 방식)
+		RedisCacheConfiguration companySearchConfiguration = RedisCacheConfiguration
+			.defaultCacheConfig()
+			.disableCachingNullValues()
+			.entryTtl(COMPANY_SEARCH_TTL)
+			.computePrefixWith(CacheKeyPrefix.simple())
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
 
 		return RedisCacheManager
 			.builder(redisConnectionFactory)
-			.cacheDefaults(configuration)
+			.cacheDefaults(defaultConfiguration)
+			.withCacheConfiguration(COMPANY_CACHE, companyConfiguration)
+			.withCacheConfiguration(COMPANY_SEARCH_CACHE, companySearchConfiguration)
 			.build();
 	}
 }
