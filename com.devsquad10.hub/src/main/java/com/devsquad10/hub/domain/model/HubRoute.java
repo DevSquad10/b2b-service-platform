d73c1799bb808789a9e01ac28dddbee3377d2b16
aerhergag00 <jwlee101500@gmail.com>
2025-03-17 23:25:15 +0900

    feat: #49 HubRoute Entity

---
+++ com.devsquad10.hub/src/main/java/com/devsquad10/hub/domain/model/HubRoute.java
@@ -0,0 +1,102 @@
+package com.devsquad10.hub.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "p_hub_route")
+@SQLRestriction("deleted_at IS NULL")
+// TODO: Audit Fields 분리
+@EntityListeners(AuditingEntityListener.class)
+public class HubRoute {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	private Double distance;
+
+	private Integer duration;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "departure_hub_id", nullable = false)
+	private Hub departureHub;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "destination_hub_id", nullable = false)
+	private Hub destinationHub;
+
+	// TODO: Audit Fields 분리
+	@Column(name = "created_at", nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt;
+
+	@Column(name = "created_by", nullable = false, updatable = false)
+	private UUID createdBy;
+
+	@Column(name = "updated_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt;
+
+	@Column(name = "updated_by")
+	private UUID updatedBy;
+
+	@Column(name = "deleted_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime deletedAt;
+
+	@Column(name = "deleted_by")
+	private UUID deletedBy;
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+
+		// TODO: created_by
+		this.createdBy = UUID.randomUUID();
+	}
+
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+
+		// TODO: updated_by
+	}
+
+	public void update(Double distance, Integer duration) {
+		this.distance = distance;
+		this.duration = duration;
+	}
+
+	public void delete(UUID deletedBy) {
+		this.deletedAt = LocalDateTime.now();
+		this.deletedBy = deletedBy;
+	}
+}
+
