5fe48757e0784fbb72b4e744d2938aeedc69caf6
minji <zz8168@naver.com>
2025-03-17 17:19:23 +0900

    feat: #7 shipping/shippingHistory search 구현 - queyrDSL 사용 - shipping: 출발허브ID,업체담당자ID,상태 조회 - shippingHistory(컨트롤러 구현X): 배송ID,배송담당자ID,상태 조회

--- com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/domain/repository/ShippingHistoryRepository.java
+++ com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/domain/repository/ShippingHistoryRepository.java
@@ -1,8 +1,12 @@
 package com.devsquad10.shipping.domain.repository;
 
+import org.springframework.data.domain.Page;
+
 import com.devsquad10.shipping.domain.model.ShippingHistory;
 
 public interface ShippingHistoryRepository {
 
 	ShippingHistory save(ShippingHistory shippingHistory);
+
+	Page<ShippingHistory> findAll(String q, String category, int page, int size, String sort, String order);
 }
