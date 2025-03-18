0e8b24757079034326384376368a860fcb89b4b6
minji <zz8168@naver.com>
2025-03-19 04:45:39 +0900

    comment: #7 TODO 및 주석 작성

--- com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/domain/enums/ShippingStatus.java
+++ com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/domain/enums/ShippingStatus.java
@@ -1,11 +1,11 @@
 package com.devsquad10.shipping.domain.enums;
 
 public enum ShippingStatus {
-	HUB_WAIT("HUB_WAIT"),
-	HUB_TRNS("HUB_TRNS"),
-	HUB_ARV("HUB_ARV"),
-	COM_TRNS("COM_TRNS"),
-	DLV_CMP("DLV_CMP");
+	HUB_WAIT("HUB_WAIT"), // 허브 대기중
+	HUB_TRNS("HUB_TRNS"), // 허브 이동중
+	HUB_ARV("HUB_ARV"), // 목적지 허브 도착
+	COM_TRNS("COM_TRNS"), // 업체 이동중
+	DLV_CMP("DLV_CMP"); // 배송완료
 
 	private final String status;
 
