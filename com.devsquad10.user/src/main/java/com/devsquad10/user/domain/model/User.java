24049c8a0219a1d81cffc30960a5e53fd1e70857
josephuk77 <josephuk77@gmail.com>
2025-03-18 14:12:57 +0900

    #32feat :  User entity, enum 생성
    설계한 테이블에 맞게 entity를 생성

---
+++ com.devsquad10.user/src/main/java/com/devsquad10/user/domain/model/User.java
@@ -0,0 +1,44 @@
+package com.devsquad10.user.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "p_users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false, unique = true)
+	private String slackId;
+
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
+	private UserRoleEnum role;
+
+	@Column
+	private String currentAddress;
+
+}
