e8fef99772277499c6a0932dddf8d635318f92b4
jjsh0208 <jjsh0208@gmail.com>
2025-03-18 11:45:52 +0900

    refactor : #2 Company RedisCachingConfig 직렬화 설정 변경
    - jdk 직렬화에서 jacksjon2json 방식으로 변경

--- com.devsquad10.company/src/main/java/com/devsquad10/company/infrastructure/config/redis/RedisCachingConfig.java
+++ com.devsquad10.company/src/main/java/com/devsquad10/company/infrastructure/config/redis/RedisCachingConfig.java
@@ -9,9 +9,11 @@ import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.devsquad10.company.application.dto.CompanyResDto;
 
 @Configuration
 @EnableCaching
@@ -27,8 +29,10 @@ public class RedisCachingConfig {
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 		// 이진 직렬화 방식 적용
-		JdkSerializationRedisSerializer redisSerializer = new JdkSerializationRedisSerializer();
 
+		Jackson2JsonRedisSerializer<CompanyResDto> companySerializer = new Jackson2JsonRedisSerializer<>(
+			CompanyResDto.class);
+		
 		// 기본값 캐시 설정 (2분 TTL, JSON 직렬화 방식)
 		RedisCacheConfiguration defaultConfiguration = RedisCacheConfiguration
 			.defaultCacheConfig()
@@ -36,7 +40,7 @@ public class RedisCachingConfig {
 			.entryTtl(DEFAULT_TTL) // 기본 유지 시간 120초
 			.computePrefixWith(CacheKeyPrefix.simple())
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(companySerializer));
 
 		// companyCache 에 대한 캐시 설정 (10분 TTL, JSON 직렬화 방식)
 		RedisCacheConfiguration companyConfiguration = RedisCacheConfiguration
@@ -45,7 +49,7 @@ public class RedisCachingConfig {
 			.entryTtl(COMPANY_TTL)
 			.computePrefixWith(CacheKeyPrefix.simple())
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(companySerializer));
 
 		// companySearchCache 에 대한 캐시 설정 (1시간 TTL, JSON 직렬화 방식)
 		RedisCacheConfiguration companySearchConfiguration = RedisCacheConfiguration
@@ -54,7 +58,7 @@ public class RedisCachingConfig {
 			.entryTtl(COMPANY_SEARCH_TTL)
 			.computePrefixWith(CacheKeyPrefix.simple())
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(companySerializer));
 
 		return RedisCacheManager
 			.builder(redisConnectionFactory)
