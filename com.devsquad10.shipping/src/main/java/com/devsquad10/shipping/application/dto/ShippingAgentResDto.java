0aa9ce09cc3d007d1021c7dc1c6dcad8d70cafd4
minji <zz8168@naver.com>
2025-03-18 15:24:35 +0900

    fix: #59 shippingAgent 응답dto 생성

---
+++ com.devsquad10.shipping/src/main/java/com/devsquad10/shipping/application/dto/ShippingAgentResDto.java
@@ -0,0 +1,31 @@
+package com.devsquad10.shipping.application.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import com.devsquad10.shipping.domain.enums.ShippingAgentType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShippingAgentResDto implements Serializable {
+
+	private UUID id;
+
+	private UUID hubId;
+
+	private String shippingManagerSlackId;
+
+	private ShippingAgentType type;
+
+	// TODO: 배정에 사용할 예정, 조회될 때 front 에서 불필요
+	private Integer shippingSequence;
+
+	private Boolean isTransit;
+}
