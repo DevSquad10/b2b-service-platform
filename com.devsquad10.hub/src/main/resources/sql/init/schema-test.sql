9ab7feb6192cbbc7eb50ddfd63ee51e7d2ec2de0
aerhergag00 <jwlee101500@gmail.com>
2025-03-20 10:59:42 +0900

    feat: #76 초기 임의의 데이터

--- com.devsquad10.hub/src/main/resources/sql/init/schema-test.sql
+++ com.devsquad10.hub/src/main/resources/sql/init/schema-test.sql
@@ -12,3 +12,18 @@ CREATE TABLE IF NOT EXISTS p_hub (
     deleted_at TIMESTAMP,
     deleted_by UUID
 );
+
+-- 허브간 연결 정보 테이블
+CREATE TABLE IF NOT EXISTS p_hub_connection (
+    id UUID PRIMARY KEY,
+    hub_id UUID NOT NULL REFERENCES p_hub(id),
+    connected_hub_id UUID NOT NULL REFERENCES p_hub(id),
+    weight INTEGER NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL,
+    created_by UUID NOT NULL,
+    updated_at TIMESTAMP,
+    updated_by UUID,
+    deleted_at TIMESTAMP,
+    deleted_by UUID
+);
