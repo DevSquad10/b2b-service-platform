7b2a04d21ed9014a20a1c41bf5643419e4ccd590
jjsh0208 <jjsh0208@gmail.com>
2025-03-21 16:02:19 +0900

    feat: #25 Order 주문 수정/삭제에 따른 배송 이벤트 세팅
    - updateRequest, updateResponse 에 맞는 change, queue 세팅
    - queue 등록 및 responseListener 세팅

--- com.devsquad10.order/src/main/java/com/devsquad10/order/application/endpoint/OrderEndPoint.java
+++ com.devsquad10.order/src/main/java/com/devsquad10/order/application/endpoint/OrderEndPoint.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import com.devsquad10.order.application.dto.message.ShippingCreateResponse;
+import com.devsquad10.order.application.dto.message.ShippingUpdateResponse;
 import com.devsquad10.order.application.dto.message.StockDecrementMessage;
 import com.devsquad10.order.application.service.OrderEventService;
 
@@ -34,4 +35,10 @@ public class OrderEndPoint {
 			orderEventService.retryCreateShipping(shippingCreateResponse);
 		}
 	}
+
+	@RabbitListener(queues = "${shippingMessage.queue.shipping_update.response}")
+	public void handlerShippingUpdateResponse(ShippingUpdateResponse updateResponse) {
+
+	}
+
 }
