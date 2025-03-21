5a9ffcdaf96cc40a99dd918cf2f3c57fa8139e7f
josephuk77 <josephuk77@gmail.com>
2025-03-21 10:58:39 +0900

    #94 feat : FeignClient 회원가입 유저정보 조회 구현
    FeignClient를 사용해 배송자 회원가입을 할때 허브배송자 인지 아니면 업체 배송자인지 정할 수 있게 API요청 구현
    다른 모듈에서 유저정보를 조회 할 수 있게 구현

---
+++ com.devsquad10.user/src/main/java/com/devsquad10/user/infrastructure/client/ShippingClient.java
@@ -0,0 +1,14 @@
+package com.devsquad10.user.infrastructure.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.devsquad10.user.application.dto.ShippingAgentFeignClientPostRequest;
+
+@FeignClient(name = "shippingAgent", url = "http://localhost:19098/api/shipping-agent")
+public interface ShippingClient {
+
+	@PostMapping
+	void createShippingAgent(@RequestBody ShippingAgentFeignClientPostRequest request);
+}
