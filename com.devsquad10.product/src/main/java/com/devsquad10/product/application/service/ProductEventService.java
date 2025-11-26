35078864a70c3dd39f9169dbc61b83da69be4698
jjsh0208 <jjsh0208@gmail.com>
2025-11-26 15:54:15 +0900

    feat : #185 <Product> 재고 차감의 동시성 제어를 위한 비관락 설정

--- com.devsquad10.product/src/main/java/com/devsquad10/product/application/service/ProductEventService.java
+++ com.devsquad10.product/src/main/java/com/devsquad10/product/application/service/ProductEventService.java
@@ -40,7 +40,7 @@ public class ProductEventService {
 		UUID targetProductId = stockDecrementMessage.getProductId();
 		int orderQuantity = stockDecrementMessage.getQuantity();
 
-		Product product = productRepository.findByIdAndDeletedAtIsNull(targetProductId)
+		Product product = productRepository.findByIdWithLock(targetProductId)
 			.orElseThrow(() -> {
 				log.error("재고 차감 실패 - 상품 ID: {}를 찾을 수 없습니다.", targetProductId);
 				return new ProductNotFoundException("Product Not Found By Id :" + targetProductId);
