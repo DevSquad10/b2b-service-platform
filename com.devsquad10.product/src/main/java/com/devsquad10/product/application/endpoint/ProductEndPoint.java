b84e6854e596727e832e8caae938f3e0fc35dcdc
jjsh0208 <jjsh0208@gmail.com>
2026-05-25 13:16:41 +0900

    feat: #191 <Product> 다중 큐 리스너 구현
    - 3개의 재고 차감 리스너 별도 구현
    - 리스너 별 log 처리

--- com.devsquad10.product/src/main/java/com/devsquad10/product/application/endpoint/ProductEndPoint.java
+++ com.devsquad10.product/src/main/java/com/devsquad10/product/application/endpoint/ProductEndPoint.java
@@ -1,33 +1,48 @@
 package com.devsquad10.product.application.endpoint;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
 import com.devsquad10.product.application.dto.message.StockDecrementMessage;
 import com.devsquad10.product.application.dto.message.StockReversalMessage;
 import com.devsquad10.product.application.service.ProductEventService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class ProductEndPoint {
 
-	private final ProductEventService productEventService;
+    private final ProductEventService productEventService;
 
-	@RabbitListener(queues = "${stockMessage.queue.stock.request}", concurrency = "20")
-	public void handleStockDecrementRequest(StockDecrementMessage stockDecrementMessage) {
-		log.info("재고 차감 요청 수신 - 상품 ID: {}, 차감 수량: {}", stockDecrementMessage.getProductId(),
-			stockDecrementMessage.getQuantity());
-		productEventService.decreaseStock(stockDecrementMessage);
-	}
+    // 1번 재고차감 파티션 큐 전담 리스너
+    @RabbitListener(queues = "${stockMessage.queue.stock.request}.1", concurrency = "1")
+    public void handleStockDecrementRequestPartition1(StockDecrementMessage stockDecrementMessage) {
+        log.info("재고 차감 요청 수신 - 1번 리스너 상품 ID: {}, 차감 수량: {}", stockDecrementMessage.getProductId(),
+                stockDecrementMessage.getQuantity());
+        productEventService.decreaseStock(stockDecrementMessage);
+    }
 
-	@RabbitListener(queues = "${stockMessage.queue.stockRecovery.request}", concurrency = "1")
-	public void handlerStockRecoveryRequest(StockReversalMessage stockReversalMessage) {
-		log.info("재고 복원 요청 수신 - 상품 ID: {}, 복원 수량: {}", stockReversalMessage.getProductId(),
-			stockReversalMessage.getQuantity());
-		productEventService.recoveryStock(stockReversalMessage);
-	}
+    // 2번 재고차감 파티션 큐 전담 리스너
+    @RabbitListener(queues = "${stockMessage.queue.stock.request}.2", concurrency = "1")
+    public void handleStockDecrementRequestPartition2(StockDecrementMessage stockDecrementMessage) {
+        log.info("재고 차감 요청 수신 - 2번 리스너 상품 ID: {}, 차감 수량: {}", stockDecrementMessage.getProductId(),
+                stockDecrementMessage.getQuantity());
+        productEventService.decreaseStock(stockDecrementMessage);
+    }
+
+    // 3번 재고차감 파티션 큐 전담 리스너
+    @RabbitListener(queues = "${stockMessage.queue.stock.request}.3", concurrency = "1")
+    public void handleStockDecrementRequestPartition3(StockDecrementMessage stockDecrementMessage) {
+        log.info("재고 차감 요청 수신 - 3번 리스너 상품 ID: {}, 차감 수량: {}", stockDecrementMessage.getProductId(),
+                stockDecrementMessage.getQuantity());
+        productEventService.decreaseStock(stockDecrementMessage);
+    }
+
+    @RabbitListener(queues = "${stockMessage.queue.stockRecovery.request}", concurrency = "1")
+    public void handlerStockRecoveryRequest(StockReversalMessage stockReversalMessage) {
+        log.info("재고 복원 요청 수신 - 상품 ID: {}, 복원 수량: {}", stockReversalMessage.getProductId(),
+                stockReversalMessage.getQuantity());
+        productEventService.recoveryStock(stockReversalMessage);
+    }
 }
