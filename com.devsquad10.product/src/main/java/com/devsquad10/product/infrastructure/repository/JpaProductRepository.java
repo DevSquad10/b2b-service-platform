fbbcd631959626eda1dc19b4adbead629e5af25f
jjsh0208 <jjsh0208@gmail.com>
2026-05-27 17:59:02 +0900

    feat: #191 <Product> 비관락 메서드 삭제

--- com.devsquad10.product/src/main/java/com/devsquad10/product/infrastructure/repository/JpaProductRepository.java
+++ com.devsquad10.product/src/main/java/com/devsquad10/product/infrastructure/repository/JpaProductRepository.java
@@ -1,27 +1,14 @@
 package com.devsquad10.product.infrastructure.repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import com.devsquad10.product.domain.model.Product;
 import com.devsquad10.product.domain.repository.ProductRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
-import jakarta.persistence.LockModeType;
+import java.util.UUID;
 
 @Repository
 public interface JpaProductRepository
-	extends JpaRepository<Product, UUID>, QuerydslPredicateExecutor<Product>, ProductRepository {
-
-	@Override
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
-	@Query("SELECT p FROM Product p WHERE p.id = :productId AND p.deletedAt IS NULL")
-	Optional<Product> findByIdWithLock(@Param("productId") UUID productId);
-
+        extends JpaRepository<Product, UUID>, QuerydslPredicateExecutor<Product>, ProductRepository {
 }
